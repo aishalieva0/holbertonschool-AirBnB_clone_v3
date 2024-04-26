eb2e3d546999cd26990da7f2bfdf5d85fe428456
aishalieva0 <aysalieva6@gmail.com>
2024-04-26 17:53:30 +0400

    registered blueprint

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ from api.v1.views import app_views
 
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
 
 
 @app.teardown_appcontext
@@ -12,4 +13,4 @@ def tearDown(self):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, threaded=True)
+    app.run(host="0.0.0.0", port=5000, threaded=True)
