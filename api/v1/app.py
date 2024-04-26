0045883ea329704ee82c8e48e427d47165f266e1
aishalieva0 <aysalieva6@gmail.com>
2024-04-26 19:14:09 +0400

    fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,7 +11,7 @@ app.register_blueprint(app_views)
 
 @app.errorhandler(404)
 def not_found(error):
-    return jsonify({"error": "Not found"}, 404)
+    return jsonify({"error": "Not found"}), 404
 
 
 @app.teardown_appcontext
